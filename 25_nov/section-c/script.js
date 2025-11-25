145c74a823ec685a6e35690d9242c24fab2da2b9
mozakir178 <mozakir178@gmail.com>
2025-11-25 14:24:13 +0530

    in class code

---
+++ 25_nov/section-c/script.js
@@ -0,0 +1,115 @@
+// function
+// array
+// object
+
+// function
+
+// let num1 = 10 ;
+// if(num1%2==0) {
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+// let num2 = 20 ;
+// if(num2%2==0) {
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+// function checkEven(num) {
+//   if (num % 2 == 0) {
+//     console.log("Even");
+//   } else {
+//     console.log("Odd");
+//   }
+// }
+// checkEven(20);
+// checkEven(31);
+
+
+// function checkEven(num) {
+//   if (num % 2 == 0) {
+//     console.log("Even");
+//     return true ;
+//   } else {
+//     return false
+//     console.log("Odd");
+//     // return false
+//   }
+// }
+// let result = checkEven(31);
+// console.log(result)
+
+
+// const arr = [1,"ravi",true, null];
+// arr = [1,2,3]
+//append - push 
+// arr.push(20);
+// arr.push([1,2,3])
+
+// console.log(arr.length); 
+// console.log(arr[arr.length-1])
+
+// let removed = arr.pop()
+// console.log(arr.length); 
+// console.log(arr[arr.length-1])
+// console.log("removed", removed); 
+// console.log(arr)
+// arr[3] = "upadted";
+// console.log(arr)
+
+
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+// console.log(arr)
+// arr.unshift(10)
+// console.log(arr)
+
+// const arr = [1,"ravi",true, null];
+// // for i in range (0,len(arr))
+
+// // i - start = 0
+// // i < len (arr)
+// // i - i+1
+
+
+// for( let i = 0   ; i < arr.length  ; i = i+1  ){
+//     console.log(arr[i]);
+// }
+
+
+// let person = {
+//     name:"Ravi",
+//     age:17,
+//     greet: function(){
+//         console.log("Hello");
+//     }
+// };
+
+// console.log(person.name)
+
+// // console.log(person.age)
+// console.log(person["age"])
+
+
+// person.greet()
+// console.log(person.greet())
+
+
+let person = {
+    name:"Ravi",
+    age:17,
+    greet: function(){
+        console.log("Hello");
+    }
+};
+console.log(person)
+
+person.lastname = "Kumar"
+console.log(person)
